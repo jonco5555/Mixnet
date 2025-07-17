7015e291d55ab676b4e54a45ab1bc4bda5b3588a
Yonatan Cohen <jonco5555@gmail.com>
2025-07-17 22:48:59 +0300

    .

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ echo "Services started. Waiting..."
 
 sleep 5 # let the clients send some dummies
 docker-compose exec client_1 /app/.venv/bin/mixnet prepare-message --message "Hello from client_1 to client_2" --sender-id client_1 --recipient-id client_2
-sleep 2 # let the message be processed
+sleep 4 # let the message be processed
 output=$(docker-compose exec -T client_2 /app/.venv/bin/mixnet poll-messages)
 echo "Output from client_2:"
 echo $output
 if [[ $output == *"Hello from client_1 to client_2"* ]]; then
     docker-compose exec client_2 /app/.venv/bin/mixnet prepare-message --message "Hello from client_2 to client_1" --sender-id client_2 --recipient-id client_1
 fi
-sleep 2 # let the message be processed
+sleep 4 # let the message be processed
 output=$(docker-compose exec -T client_1 /app/.venv/bin/mixnet poll-messages)
 echo "Output from client_1:"
 echo $output
